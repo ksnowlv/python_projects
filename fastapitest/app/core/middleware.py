31df613a451d6406e3ea5325096b46d0f1f62e73
lvwei <lvwei220406@credithc.com>
2023-11-24 18:05:48 +0800

    fastapitest:开启gzip压缩

--- fastapitest/app/core/middleware.py
+++ fastapitest/app/core/middleware.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from app.core.xlogger import LogMiddleware
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 
 def init_middleware(app: FastAPI):
     app.add_middleware(LogMiddleware)
@@ -13,4 +14,7 @@ def init_middleware(app: FastAPI):
         allow_methods=["*"],  # 设置允许的 HTTP 方法，可以是 '*' 表示允许所有方法，也可以是列表
         allow_headers=["*"],  # 设置允许的 HTTP 头部，可以是 '*' 表示允许所有头部，也可以是列表
     )
-
+    # 开启gzip压缩
+    # 当请求的头信息 Accept-Encoding 字段带有"gzip"时，GZipMiddleware负责完成相应的返回结果处理。
+    # GZipMiddleware 支持 minimum_size 参数：当返回结果大小小于指定值时不启用压缩。(单位为字节，默认值为500)
+    app.add_middleware(GZipMiddleware, minimum_size=1000)
