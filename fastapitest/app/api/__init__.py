0230445fb524e2e0a9d3c41c33b0ea6f5cd77a59
lvwei <lvwei220406@credithc.com>
2023-11-28 10:38:49 +0800

    fastapitest：新增kafka接口使用api示例

--- fastapitest/app/api/__init__.py
+++ fastapitest/app/api/__init__.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, FastAPI
 from .files import filerouter
 from .users import userrouter
 from .redis import redisrouter
+from .kafka import kafkarouter
 
 
 def init_routers(app: FastAPI):
@@ -11,5 +12,6 @@ def init_routers(app: FastAPI):
     api_routes.include_router(userrouter.router)
     api_routes.include_router(filerouter.router)
     api_routes.include_router(redisrouter.router)
+    api_routes.include_router(kafkarouter.router)
     app.include_router(api_routes)
     return api_routes
